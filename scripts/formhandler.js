770f4b5bc141965e698b36359e549a7c992f9ae4
kiryu <hsiehdjames@csu.fullerton.edu>
2017-03-21 20:35:41 -0700

    first commit

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -1,7 +1,10 @@
+var strength = 30;
 (function(window) {
     'use strict';
     var App = window.App || {};
     var $ = window.jQuery;
+    var emailList = [];
+    $('#powerBody').hide();
 
     function FormHandler(selector) {
         if (!selector) {
@@ -28,13 +31,19 @@
                 data[item.name] = item.value;
                 console.log(item.name + ' is ' + item.value);
             });
-            if (data['size'] == 'coffee-zilla' && data['flavor'] != '' && data['strength'] == '100') {
+            if (data['size'] == 'coffee-zilla' && data['flavor'] != '' && data['strength'] == '100' && data['email'] != '' && emailList.indexOf(data['email']) == -1) {
                 $('#myModal').modal('show');
                 var power = '';
-                $('input[name="power"]').on('click', function() {
-                    power = this.value;
+                $('input[name="pFreeze"]').on('click', function() {
+                    power += this.value + ' ';
                 });
-
+                $('input[name="pInv"]').on('click', function() {
+                    power += this.value + ' ';
+                });
+                $('input[name="pMind"]').on('click', function() {
+                    power += this.value + ' ';
+                });
+                emailList.push(data['email']);
                 $('#btSave').unbind().click(function() {
                     console.log('power is ' + power);
                     data['power'] = power;
@@ -42,6 +51,30 @@
                     console.log(data);
                     fn(data);
                 });
+                strength = 30;
+                this.reset();
+                this.elements[0].focus();
+                $('#strengthTxt')[0].textContent = '30';
+                $('#strengthTxt').css('color', 'green');
+            } else if (emailList.indexOf(data['email']) != -1) {
+                var p = '';
+                $('input[name="ppFreeze"]').on('click', function() {
+                    p += this.value + ' ';
+                });
+                $('input[name="ppInv"]').on('click', function() {
+                    p += this.value + ' ';
+                });
+                $('input[name="ppMind"]').on('click', function() {
+                    p += this.value + ' ';
+                });
+                $('#btSave').unbind().click(function() {
+                    console.log('power is ' + p);
+                    data['power'] = p;
+                    $('#myModal').modal('hide');
+                    console.log(data);
+                    fn(data);
+                });
+                strength = 30;
                 this.reset();
                 this.elements[0].focus();
                 $('#strengthTxt')[0].textContent = '30';
@@ -53,6 +86,51 @@
                 this.elements[0].focus();
                 $('#strengthTxt')[0].textContent = '30';
                 $('#strengthTxt').css('color', 'green');
+            }
+        });
+    };
+
+    FormHandler.prototype.addInputHandler = function(fn) {
+        console.log('Setting input handler for form');
+        this.$formElement.on('input', '[name="emailAddress"]', function(event) {
+            console.log(event.target.value + ', email');
+            var emailAddress = event.target.value;
+            var message = '';
+            if (fn(emailAddress)) {
+                event.target.setCustomValidity('');
+            } else {
+                message = emailAddress + ' is not an authorized email address!';
+                event.target.setCustomValidity(message);
+            }
+        });
+    };
+
+    FormHandler.prototype.addDecafHandler = function(fn) {
+        console.log('decaf validation '+strength);
+        var coffeeName = '';
+        var message = 'Strength too strong for decaf!';
+        this.$formElement.on('input', '[name="coffee"]', function(event) {
+            coffeeName = event.target.value;
+            console.log($('input[name="coffee"]')[0]);
+            if (fn(coffeeName, strength)) {
+                console.log('text okay');
+                event.target.setCustomValidity('');
+                $('input[name="strength"]')[0].setCustomValidity('');
+            } else {
+                console.log('text not okay');
+                event.target.setCustomValidity(message);
+            }
+        });
+        this.$formElement.on('input', '[name="strength"]', function(event) {
+            strength = event.target.value;
+            console.log('asdfasdf' + coffeeName + ':' + strength);
+            if (fn(coffeeName, strength)) {
+                console.log('number okay');
+                event.target.setCustomValidity('');
+                $('input[name="coffee"]')[0].setCustomValidity('');
+            } else {
+                console.log('number not okay');
+                event.target.setCustomValidity(message);
             }
         });
     };
@@ -72,6 +150,7 @@
 
         //  console.log('currentColor: ' + $('#strengthTxt').css('color'));
     });
+
 
     App.FormHandler = FormHandler;
     window.App = App;
---
+++ scripts/validation.js
@@ -0,0 +1,21 @@
+(function(window) {
+    'use strict';
+    var App = window.App || {};
+    var Validation = {
+        isCompanyEmail: function(email) {
+            return /.+@bignerdranch\.com$/.test(email);
+        },
+        decafValidation: function(word, strength) {
+            console.log('validation: ' + word + ',' + strength);
+            if (word.includes('decaf') && strength >= 20) {
+                console.log('too strong');
+                return false;
+            } else {
+                console.log('ok');
+                return true;
+            }
+        }
+    };
+    App.Validation = Validation;
+    window.App = App;
+})(window);
